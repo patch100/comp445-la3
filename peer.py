914a25b5582b6b0a3c64c83fe590ca14c786bd48
patch100 <patch.93@gmail.com>
2017-03-22 15:40:18 -0400

    Added LEAVE command

--- peer.py
+++ peer.py
@@ -9,15 +9,16 @@ def run_sender(username, host, port, q):
     sender.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     sender.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     try:
-        print("Chat away!")
+        join_message = write_app_message(username, "JOIN", "has joined chat")
+        sender.sendto(join_message, (host, port))
         while True:
             user_message = raw_input()
-            if user_message == "q":
+            if user_message == "/leave":
                 q.put("quit")
-                app_message = write_app_message(username, "left chat")
+                app_message = write_app_message(username, "LEAVE", "left chat")
                 sender.sendto(app_message, (host, port))
                 break
-            app_message = write_app_message(username, user_message)
+            app_message = write_app_message(username, "TALK", user_message)
             sender.sendto(app_message, (host, port))
             sleep(1)
     except error, msg:
@@ -36,19 +37,25 @@ def run_receiver(host, port, q):
                     q.task_done()
                     break
             data, addr = receiver.recvfrom(1024)  # buffer size is 1024 bytes
-            sender, message = read_app_message(data)
-            print "[{}]: {}".format(sender, message)
+            sender, command, message = read_app_message(data)
+
+            if command == "TALK":
+                print "[{}]: {}".format(sender, message)
+            elif command == "JOIN":
+                print "{} joined!".format(sender)
+            elif command == "LEAVE":
+                print "{} left!".format(sender)
     except error, msg:
         print msg
     finally:
         receiver.close()
 
-def write_app_message(username, message):
-    return "user: {}\nmessage: {}\n\n".format(username,message)
+def write_app_message(username, message, command):
+    return "user: {}\ncommand: {}\nmessage: {}\n\n".format(username,message, command)
 
 def read_app_message(data):
-    search = re.search('user: (\w+)\s*message: ([\w \S]*)\n\n', data)
-    return (search.group(1), search.group(2))
+    search = re.search('user: (\w+)\s*command: (TALK|JOIN|LEAVE)\s*message: ([\w \S]*)\n\n', data)
+    return (search.group(1), search.group(2), search.group(3))
 
 username = raw_input("Please choose a username: ")
 
